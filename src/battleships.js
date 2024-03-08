9513559d432a9f8ba06b37148d3eaf923d8bddd3
Bartlomiej Bogdan Szarlej <bartlomiej-bogdan.szarlej@dolphin-it.de>
2024-03-08 14:57:16 +0100

    upload

--- src/battleships.js
+++ src/battleships.js
@@ -3,7 +3,7 @@ const ROWS = 10;
 const COLS_LABELS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
 const TILE_WIDTH = 45;
 const TILE_HEIGHT = 45;
-const REQUIRED_SHIPS = [5, 4, 4, 3, 3, 3, 2, 2, 2, 2]
+const REQUIRED_SHIPS = [5, 4, 4, 3, 3, 3, 2, 2, 2, 2];
 
 const GameState = {
     initial: 0,
@@ -15,11 +15,11 @@ class Game {
     #lastHover = null;
     #players = [new Player(), new Player()];
     #currentPlayer = this.#players[0];
-    #state = GameState.initial;
+    #state = null;
 
     constructor() {
-        document.addEventListener('keydown', (event) => this.#toggleShipRotation(event));
         for(let i = 0; i < this.#players.length; i++) {
+            this.#players[i].name = `Player ${i + 1}`;
             let playerContainer = document.getElementById('playerContainer');
             playerContainer.appendChild(this.#createPlayerBoard(i));
 
@@ -46,24 +46,53 @@ class Game {
                     var tile = document.createElement('div');
                     tile.className = 'tile';
                     tile.id = `${i}-${k}-${j}`;
-                    tile.onmouseover = (event) => this.#onTileMouseOver(event);
-                    tile.onmouseleave = (event) => this.#onTileMouseLeave(event);
                     tile.onclick = (event) => this.#onTileClicked(event);
+                    tile.onmouseover = (event) => this.#onTileMouseOver(event);
+                    tile.onmouseleave = () => this.#clearPreview();
                     tile.style.top = `${j * TILE_HEIGHT}px`;
                     tile.style.left = `${k * TILE_WIDTH}px`;
                     tile.style.height = `${TILE_HEIGHT}px`;
                     tile.style.width = `${TILE_WIDTH}px`;
                     tile.style.lineHeight = `${TILE_HEIGHT}px`;
                     tile.style.textAlign = 'center';
-                    battlefield.appendChild(tile);
+                    battlefield.appendChild(tile);                    
                 }
             }
         }
     }
 
-    updateState(state) {
+    run() {
+        this.#setState(GameState.initial);
+    }
+
+    #setState(state) {
         if(this.#state == state) return;
+        if(state == GameState.preparation) {
+            this.#showDialog('Game Started', `${this.#currentPlayer.name} turn`);
+            document.addEventListener('keydown', (event) => this.#toggleShipRotation(event));
+        }
+        let stateBtn = document.getElementById('state-btn');
+        stateBtn.innerHTML = state == GameState.initial ? 'Start' : 'Reset';
+        stateBtn.style.backgroundColor = state == GameState.initial ? 'rgba(0, 255, 0, 0.5)' : 'rgba(255, 0, 0, 0.5)';
+        stateBtn.onclick = () => this.#setState( state == GameState.initial ? GameState.preparation : GameState.initial );
         this.#state = state;
+    }
+
+    #showDialog(title, content) {
+        let dialog = document.createElement('dialog');
+        dialog.className = 'column main-axis-space-between cross-axis-center';
+
+        dialog.innerHTML += `<h2>${title}</h2>`;
+        dialog.innerHTML += `<h3>${content}</h3>`;
+        
+        document.body.appendChild(dialog);
+        dialog.showModal();
+        dialog.addEventListener('animationend', () => {
+            setTimeout(() => {
+                dialog.classList.add('hide');
+                dialog.addEventListener('animationend', () => dialog.remove());
+            }, 2000);
+        })
     }
 
     #createPlayerBoard(index) {
@@ -78,7 +107,7 @@ class Game {
         column.className = 'column cross-axis-center';
 
         let playerLabel = document.createElement('h2');
-        playerLabel.innerHTML = `Player ${index + 1}`;
+        playerLabel.innerHTML = this.#players[index].name;
 
         let columnLabels = document.createElement('div');
         columnLabels.className = 'row col-labels';
@@ -104,29 +133,26 @@ class Game {
     #onTileMouseOver(event) {
         if(this.#state == GameState.initial) return;
         let id = event.target.id.split('-');
-        let playerIndex = this.#players.indexOf(this.#currentPlayer);
+        let player = this.#currentPlayer;
+        let playerIndex = this.#players.indexOf(player);
         if(Number(id[0]) != playerIndex) return;
         switch(this.#state){
             case GameState.preparation:
-                this.#onTileMouseLeave();
+                this.#clearPreview();
                 let column = Number(id[1]);
                 let row = Number(id[2]);
-                let nextShip = this.#currentPlayer.shipsToPlace[0];
-                let isVertical =  this.#currentPlayer.shipAlignment == Alignment.vertical;
+                let isVertical =  player.shipAlignment == Alignment.vertical;
+                let nextShip = player.nextShip();
                 let startIndex = isVertical ? row : column;
                 let endIndex = startIndex + nextShip.size;
-                nextShip.tiles = [];
+                let previewTiles = []
                 for(let i = startIndex; i < endIndex; i++) {
-                    let tile = null;
-                    tile = document.getElementById(`${playerIndex}-${column}-${row}`);
-                    for(let i = 0; i < this.#currentPlayer.shipsPlaced.length; i++)
-                        if(this.#currentPlayer.shipsPlaced[i].tiles.includes(tile)) nextShip.tiles.push(null);
-                    nextShip.tiles.push(tile);
+                    let tile = document.getElementById(`${playerIndex}-${column}-${row}`);
+                    previewTiles.push(tile);
                     isVertical ? row++ : column++;
-                    nextShip.canBePlaced = this.#canShipBePlaced(tile, this.#currentPlayer.shipsPlaced)
-                    let color = nextShip.canBePlaced ? 'lightgreen' : 'red';
-                    if(tile != null) tile.style.backgroundColor = color;
                 }
+                nextShip.tiles = previewTiles;
+                nextShip.showPreview(player.canPlaceShip() ? 'rgba(0, 255, 0, 0.5)' : 'rgba(255, 0, 0, 0.5)');
                 this.#lastHover = event.target;
                 break;
             case GameState.battle:
@@ -134,30 +160,14 @@ class Game {
         }
     }
 
-    #canShipBePlaced(tile, placedShips) {
-        if(tile == null) return false;
-        let tileId = tile.id.split('-');
-        for(let i = 0; i < placedShips.length; i++) {
-            for(let j = 0; j < placedShips[i].tiles.length; j++){
-                let id = placedShips[i].tiles[j].id.split('-');
-                if(id[1] == tileId[1] || tileId[1] - 1 == id[1] || tileId[1] + 1 == id[1] || id[2] == tileId[2] || tileId[2] - 1 == id[2] || tileId[2] + 1 == id[2])
-                    return false;
-            }
-        }
-        return true;
-    }
-
-    #onTileMouseLeave() {
-        if(!this.#currentPlayer.allShipsPlaced())
-            this.#currentPlayer.shipsToPlace[0].tiles = [];
-        let tiles = document.getElementsByClassName('tile');
-        for(let i = 0; i < tiles.length; i++){
-            tiles[i].style.backgroundColor = 'transparent';
+    #clearPreview() {
+        for(let tile of document.getElementsByClassName('tile')) {
+            tile.style.backgroundColor = 'transparent';
         }
         this.#lastHover = null;
     }
 
-   #toggleShipRotation (event) {
+   #toggleShipRotation(event) {
         if(event.key != 'r' || this.#state != GameState.preparation) return;
         let alignment = this.#currentPlayer.shipAlignment;
         this.#currentPlayer.shipAlignment = alignment == Alignment.vertical ? Alignment.horizontal : Alignment.vertical;
@@ -165,19 +175,19 @@ class Game {
     }
 
     #onTileClicked(event) {
-        let id = event.target.id.split('-');
+        let tileId = event.target.id.split('-');
         let playerIndex = this.#players.indexOf(this.#currentPlayer)
-        if(Number(id[0]) != playerIndex) return;
+        if(Number(tileId[0]) != playerIndex) return;
         switch(this.#state) {
             case GameState.initial:
                 return;
             case GameState.preparation:
-                if(this.#currentPlayer.shipsToPlace[0].canBePlaced)
+                if(this.#currentPlayer.canPlaceShip())
                     this.#currentPlayer.placeNextShip();
                 if(this.#currentPlayer.allShipsPlaced()) {
                     this.#currentPlayer = this.#nextPlayer();
-                    if(this.#currentPlayer.allShipsPlaced()) this.updateState(GameState.battle);
-                    this.#onTileMouseLeave();
+                    if(this.#currentPlayer.allShipsPlaced()) this.#setState(GameState.battle);
+                    this.#clearPreview();
                 };
                 this.#lastHover?.dispatchEvent(new CustomEvent('mouseover', {target: this.#lastHover}));
             case GameState.battle:
@@ -187,10 +197,22 @@ class Game {
 }
 
 class Player {
-    constructor() {
+    constructor(name) {
+        this.name = name;
         this.shipsToPlace = Array.from(REQUIRED_SHIPS, (shipSize, index) => new Battleship(shipSize));
         this.shipsPlaced = [];
         this.shipAlignment = Alignment.vertical;
+    }
+
+    nextShip() {
+        return this.shipsToPlace[0];
+    }
+
+    canPlaceShip() {
+        console.clear();
+        let newShip = this.nextShip();
+        if(newShip.tiles.includes(null)) return false;
+        return true;
     }
 
     allShipsPlaced() {
@@ -198,7 +220,7 @@ class Player {
     }
 
     placeNextShip() {
-        this.shipsToPlace[0].place();
+        this.nextShip().place();
         this.shipsPlaced.push(this.shipsToPlace.shift());
     }
 }
@@ -207,12 +229,17 @@ class Battleship {
     constructor(size) {
         this.size = size;
         this.tiles = [];
-        this.canBePlaced = false;
     }
 
     place() {
-        for(let i = 0; i < this.tiles.length; i++) {
-            this.tiles[i].innerHTML = 'X';
+        for(let tile of this.tiles) {
+            tile.innerHTML = 'X';
+        }
+    }
+
+    showPreview (color) {
+        for(let tile of this.tiles) {
+            if(tile != null) tile.style.backgroundColor = color;
         }
     }
 }
@@ -222,8 +249,10 @@ const Alignment = {
     horizontal: 1,
 }
 
-document.addEventListener('DOMContentLoaded', (_) => {
+document.addEventListener(
+    'DOMContentLoaded',
+     () => {
         let game = new Game();
-        document.getElementById('start-button').onclick = () => game.updateState(GameState.preparation);
-    }
+        game.run();
+     }
 );
